d9065d8a0a797d8c8811566b61dac8abf90d78ef
Geovane Jorge <geovane.jorge@gmail.com>
2022-01-24 18:47:18 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace DigitalInnovationOne{
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Aluno[] alunos = new Aluno[5];
+        var indiceAluno=0;
+        string opcaoUsuario = ObterOpcaoUsuario();
+
+        while (opcaoUsuario.ToUpper() != "X")
+        {
+            switch (opcaoUsuario)
+            {
+                case "1":
+                    // TODO adicionar alunos
+                    Console.WriteLine("Informe o nome do aluno");
+                    Aluno aluno = new Aluno();
+                    aluno.Nome=Console.ReadLine(); 
+                    Console.WriteLine("Informe a nota do aluno: ");
+                    
+                    if (decimal.TryParse(Console.ReadLine(),out decimal nota ))
+                    {
+                        aluno.Nota= nota;
+                    }
+                    else
+                    {
+                        throw new ArgumentException("Valor da nota de ser decimal");
+                    }
+
+                    alunos[indiceAluno] = aluno;
+                    indiceAluno++;
+                    
+                    break;
+
+                case "2":
+                    //TODO Listar alunos
+                    foreach(var a in alunos)
+                    {
+                        if(!a.Nome.Equals(""))
+                        {
+                        Console.WriteLine($"ALUNO: {a.Nome} - NOTA: {a.Nota}");
+                        }
+                    }
+
+                    break;
+
+                case "3":
+                    //TODO Calcular media Geral 
+                    break;
+
+                default:
+                    throw new ArgumentOutOfRangeException();
+
+            }
+            opcaoUsuario = Console.ReadLine();
+        }
+    }
+
+    private static string ObterOpcaoUsuario()
+    {   
+        Console.WriteLine();
+        Console.WriteLine(" Informe a opção desejada:");
+        Console.WriteLine("1- Inserir novo aluno");
+        Console.WriteLine("2- Listar alunos");
+        Console.WriteLine("3- Listar alunos");
+        Console.WriteLine("X -Sair");
+        Console.WriteLine();
+
+        string opcaoUsuario = Console.ReadLine();
+        return opcaoUsuario;
+    }
+
+        public override bool Equals(object? obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string? ToString()
+        {
+            return base.ToString();
+        }
+    }
+}

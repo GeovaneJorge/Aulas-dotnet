18e40a7edcd55e2d91ac032599de6c79841b9bdb
Geovane Jorge <geovane.jorge@gmail.com>
2022-02-11 10:07:57 -0300

    Aula de Construtores

---
+++ ExemploConstrutores/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ExemploConstrutores.Models;
+
+namespace ExemploConstrutores
+{
+  class Program
+  {
+     static void Main(string[] args)
+     {
+         Pessoa p1 = new Pessoa("Geovane","Jorge");
+         p1.Apresentar();
+     }
+
+  }
+}
